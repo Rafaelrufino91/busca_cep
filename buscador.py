60ead181169fc366f9c91b5248fc39e63341d8b2
Rafael da silva rufino <39930158+Rafaelrufino91@users.noreply.github.com>
2022-11-18 13:09:40 -0300

    Update buscador.py

--- buscador.py
+++ buscador.py
@@ -1,26 +1,53 @@
 import requests
+from tkinter import *
 
-print('='*50)
-cep_corrigido = ''
-cep = str(input('Digite o cep que deseja: ')).strip()
-print('='*50)
-for l in cep:
-    if l.isnumeric():
-        cep_corrigido += l
+class main:
+    def __init__(self, master=None):
+        self.resultado = Frame(master)
+        self.resultado["pady"] = 5
+        self.resultado.pack()
 
-while True:
-    if len(cep_corrigido) < 8:
-        cep_corrigido += '0'
-    elif len(cep_corrigido) == 8:
-        break
+        self.tela = Frame(master)
+        self.tela.pack()
 
-try:
-    print(f'O cep digitado foi: {cep_corrigido}')
-    requisicao = requests.get(f'http://viacep.com.br/ws/{cep_corrigido}/json/')
-    endereco = requisicao.json()
-    rua = endereco['logradouro']
-    uf = endereco['uf']
-    cidade = endereco['localidade']
-    print(f'{rua}, {cidade} - {uf}')
-except KeyError:
-    print('Cep não encontrado ou inválido')
+        self.titulo = Label(self.tela, text='Digite o cep')
+        self.titulo.pack()
+
+        self.cep = Entry(self.tela)
+        self.cep["width"] = 12
+        self.cep.pack()
+
+        self.pesquisar = Button(self.tela)
+        self.pesquisar["text"] = "Pesquisar"
+        self.pesquisar["command"] = self.ConsultaCep
+        self.pesquisar.pack()
+
+        self.msg = Label(self.resultado, text="", font="Arial")
+        self.msg.pack()
+    
+    #metodo consulta cep
+    def ConsultaCep(self):
+        cep_corrigido = ''
+        cep = self.cep.get()
+        for l in cep:
+            if l.isnumeric():
+                cep_corrigido += l
+        while True:
+            if len(cep_corrigido) < 8:
+                cep_corrigido += '0'
+            elif len(cep_corrigido) == 8:
+                break
+        try:
+            requisicao = requests.get(f'http://viacep.com.br/ws/{cep_corrigido}/json/')
+            endereco = requisicao.json()
+            if cep:
+                rua = endereco['logradouro']
+                uf = endereco['uf']
+                cidade = endereco['localidade']
+                self.msg["text"] = rua+uf+cidade
+        except KeyError:
+            self.msg["text"] = "Cep não encontrado ou inválido"
+
+root = Tk()
+main(root)
+root.mainloop()
